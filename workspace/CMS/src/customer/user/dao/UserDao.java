aa9a70f0de806ab9477c3011c8f58cfbecd5b9c2
LeSunny <rhdtha01@gmail.com>
2018-01-08 14:35:04 +0900

    CMS v2

--- workspace/CMS/src/customer/user/dao/UserDao.java
+++ workspace/CMS/src/customer/user/dao/UserDao.java
@@ -1,34 +1,40 @@
 package customer.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import customer.user.domain.User;
 
-public class UserDao {
+public class UserDao { //테이블 하나 당 클래스 하나. user 테이블 담당.
+	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
-				"book");
-
-		PreparedStatement ps = c.prepareStatement(
-			"insert into users(id, name, password) values(?,?,?)");
+		//ClassNotFoundException : 클래스가 정의되어 있지 않거나 잘못 참조되어 찾을 수 없을 때 호출된다
+		//SQLException : SQL Server에서 경고 또는 오류를 반환할 때 throw되는 예외
+		
+		GetConnection G = new GetConnection();
+		Connection c = G.getConnection(); 
+		
+		/* 데이타 설정하는 과정 */
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		//PreparedStatement : DB에 쿼리 보내기 위한 필요 객체 statement에서 반복적인 수행을 더 쉽게 할 수 있다.
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
 
-		ps.executeUpdate();
+		ps.executeUpdate(); //삽입, 수정, 삭제와 관련된 SQL문을 실행할 때 사용.
 
 		ps.close();
 		c.close();
+		// close()를 제대로 안 할 경우, 서버가 다운될 확률이 올라간다. 메모리가 소모되기 때문
 	}
 
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = getConnection();
+		GetConnection G = new GetConnection();
+		Connection c = G.getConnection(); 
+		
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
@@ -45,32 +51,6 @@ public class UserDao {
 		c.close();
 
 		return user;
-	}
-
-
-	private Connection getConnection() throws ClassNotFoundException,
-			SQLException {
-		Connection c = getConnection();
-		return c;
-	}
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao();
-
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
-
-		dao.add(user);
-			
-		System.out.println(user.getId() + " 등록 성공");
-		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-			
-		System.out.println(user2.getId() + " 조회 성공");
 	}
 
 }
